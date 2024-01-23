c615f34644d986fbf4c7000a073e2dacb742bc81
alocke8181 <alocke_8181@yahoo.com>
2024-01-23 16:19:06 -0500

    done

--- bubble.js
+++ bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(input) {
+    for(let i = 0; i < input.length; i++){
+        for(let j=0; j < input.length; j++){
+            if(input[j]>input[j+1]){
+                let val = input[j];
+                input[j]=input[j+1];
+                input[j+1]=val;
+            }
+        }
+    }
+    return input;
+
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,33 @@
-function merge() {}
+function merge(in1, in2) {
+    const out = [];
+    let i = 0;
+    let j = 0;
+    while(i < in1.length && j < in2.length){
+        if(in1[i] < in2[j]){
+            out.push(in1[i]);
+            i++;
+        }else{
+            out.push(in2[j]);
+            j++
+        }
+    }
+    while(i < in1.length){
+        out.push(in1[i]);
+        i++;
+    }
+    while(j < in2.length){
+        out.push(in2[j]);
+        j++;
+    }
+    return out;
+}
 
-function mergeSort() {}
+function mergeSort(input) {
+    if(input.length <=1){return input};
+    const midIdx = Math.floor(input.length/2);
+    const left = mergeSort(input.slice(0,midIdx));
+    const right = mergeSort(input.slice(midIdx));
+    return merge(left,right);
+}
 
 module.exports = { merge, mergeSort};
